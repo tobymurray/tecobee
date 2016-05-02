7f4a941d8b0006f5642519a0866c624f5e39a995
Toby Murray <murray.toby@gmail.com>
2016-05-02 00:44:57 -0400

    Add minutes to historical query

--- app/controllers/runtime_report_controller.rb
+++ app/controllers/runtime_report_controller.rb
@@ -21,19 +21,19 @@ class RuntimeReportController < ApplicationController
   end
 
   def query
-    start_datetime = params["start_datetime"]
-    end_datetime = params["end_datetime"]
+    local_start_datetime = params["start_datetime"]
+    local_end_datetime = params["end_datetime"]
 
-    if start_datetime.empty? || end_datetime.empty?
+    if local_start_datetime.empty? || local_end_datetime.empty?
       render text: "Start datetime or end datetime is missing"
       return
     end
 
-    start_date = DateTime.parse(start_datetime).to_date
-    end_date = DateTime.parse(end_datetime).to_date
+    utc_start_datetime = ActiveSupport::TimeZone.new('America/New_York').local_to_utc(DateTime.parse(local_start_datetime))
+    utc_end_datetime = ActiveSupport::TimeZone.new('America/New_York').local_to_utc(DateTime.parse(local_end_datetime))
 
     @token_request_response = TokenRequestResponse.first!
-    query_string = URI.encode "#{EcobeeApi::FORMAT_JSON}&body={\"startDate\":\"#{start_date}\",\"endDate\":\"#{end_date}\",\"columns\":\"zoneCalendarEvent\",\"selection\":{\"selectionType\":\"thermostats\",\"selectionMatch\":\"310188254149\"}}"
+    query_string = URI.encode "#{EcobeeApi::FORMAT_JSON}&body={#{query_object_for_interval(utc_start_datetime, utc_end_datetime)},\"columns\":\"zoneCalendarEvent\",\"includeSensors\":\"true\",\"selection\":{\"selectionType\":\"thermostats\",\"selectionMatch\":\"310188254149\",\"includeRuntime\":\"true\"}}"
     url = "#{EcobeeApi::RUNTIME_REPORT_ROOT}?#{query_string}"
     Rails.logger.debug(url)
     begin
@@ -47,8 +47,27 @@ class RuntimeReportController < ApplicationController
         Rails.logger.debug("Error code is #{error.code} and Success code is #{ResponseCode.success.code}, message is #{error.message}")
       end
     end
-    Rails.logger.debug("Response is #{response}")
 
-    render text: response.to_json
+    Rails.logger.debug(response.to_s)
+    render text: response.to_s
+  end
+
+  def query_object_for_interval(utc_start_datetime, utc_end_datetime)
+    utc_start_date = utc_start_datetime.to_date
+    utc_start_interval = datetime_to_interval utc_start_datetime
+
+    utc_end_date = utc_end_datetime.to_date
+    utc_end_interval = datetime_to_interval utc_end_datetime
+
+    "\"startDate\":\"#{utc_start_date}\",\"startInterval\":\"#{utc_start_interval}\",\"endDate\":\"#{utc_end_date}\",\"endInterval\":\"#{utc_end_interval}\""
+  end
+
+  def datetime_to_interval(datetime)
+    num_minutes = (datetime - datetime.at_beginning_of_day) * Utils::MINUTES_PER_DAY
+    minutes_to_interval(num_minutes)
+  end
+
+  def minutes_to_interval(minutes)
+    (minutes/5).to_i
   end
 end
--- app/models/utils.rb
+++ app/models/utils.rb
@@ -1,4 +1,7 @@
 class Utils
+  MINUTES_IN_HOUR = 60
+  HOURS_IN_DAY = 24
+  MINUTES_PER_DAY = MINUTES_IN_HOUR * HOURS_IN_DAY
   def self.fahrenheit_to_celsius(degrees)
     (degrees - 32) / (9 / 5.0)
   end
